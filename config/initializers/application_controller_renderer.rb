35718cbde65496829e6af33d854b6108ad10d7da
GakuMatsuya <gaku.1237@icloud.com>
2021-01-10 09:23:03 +0000

    [Ass] secret_key

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9fbb62eed0c26cea7cff9bae806388bea01449aa463b898bc89b62375ac71c2d615b857f59f8724544c89a1300654b6fd43f65688c732d800489816fbeaa8b0b'
