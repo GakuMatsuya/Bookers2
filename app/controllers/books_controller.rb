dd1b0ce11a9dc3845e6ce1570e9b9c7cdb89348b
GakuMatsuya <gaku.1237@icloud.com>
2021-01-11 08:46:32 +0000

    [Add] validate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to books_path
+    else
+      render :index
+    end
   end
 
   def index
     @books = Book.all
     @book = Book.new
+    
   end
 
   def show
     @book = Book.find(params[:id])
+
   end
 
   def edit
