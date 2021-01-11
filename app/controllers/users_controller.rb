257f8f98e90ccaf68262adf6d6f6c7578488fbf9
GakuMatsuya <gaku.1237@icloud.com>
2021-01-11 12:15:07 +0000

    [Add] success-message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -12,10 +12,11 @@ class UsersController < ApplicationController
   def edit
    @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
       redirect_to user_path(current_user.id)
     else
       render :edit
