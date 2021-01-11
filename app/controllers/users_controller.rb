f5e22879d620a2d6e3d640de020ec9e741abe533
GakuMatsuya <gaku.1237@icloud.com>
2021-01-11 13:03:27 +0000

    [Add] 編集の制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,12 @@ class UsersController < ApplicationController
 
   def edit
    @user = User.find(params[:id])
-  end
+   if @user == current_user
+     render "edit"
+   else
+     redirect_to books_path
+   end
+  end 
 
   def update
     @user = User.find(params[:id])
